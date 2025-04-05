48086d92331ef81b5a5f584422d584a0285ff16d
Rene Baine <renebaine13@gmail.com>
2025-04-04 20:43:41 -0400

    Subtask 5 and subtask 6 updated changes

--- RentalSystem.java
+++ RentalSystem.java
@@ -32,14 +32,24 @@ public class RentalSystem {
         return instance;
     }
 
-    public void addVehicle(Vehicle vehicle) {
+    public boolean addVehicle(Vehicle vehicle) {
+        if (findVehicleByPlate(vehicle.getLicensePlate()) != null) {
+            System.out.println("Error: A vehicle with the license plate " + vehicle.getLicensePlate() + " already exists.");
+            return false;
+        }
         vehicles.add(vehicle);
         saveVehicle(vehicle);
+        return true;
     }
 
-    public void addCustomer(Customer customer) {
+    public boolean addCustomer(Customer customer) {
+        if (findCustomerById(customer.getCustomerId()) != null) {
+            System.out.println("Error: A customer with ID " + customer.getCustomerId() + " already exists.");
+            return false;
+        }
         customers.add(customer);
         saveCustomer(customer);
+        return true;
     }
 
     public void rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
